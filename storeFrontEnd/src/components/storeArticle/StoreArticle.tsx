c9c5999d3b9ffe5f3633b047ccbf4ec3664092c8
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-10-17 05:53:09 +0200

    x

--- storeFrontEnd/src/components/storeArticle/StoreArticle.tsx
+++ storeFrontEnd/src/components/storeArticle/StoreArticle.tsx
@@ -1,3 +1,4 @@
+import Carousel from "react-material-ui-carousel";
 import styles from "./StoreArticle.module.css";
 
 export type StoreArticlePros = {
@@ -7,11 +8,28 @@ export type StoreArticlePros = {
 };
 
 const StoreArticle = () => {
+  const linkes = [
+    "imgs/DALL·E 2023-10-16 09.48.25.png",
+    "imgs/DALL·E 2023-10-16 09.48.26.png",
+    "imgs/DALL·E 2023-10-16 09.48.27.png",
+    "imgs/DALL·E 2023-10-16 09.48.28.png",
+  ];
+
   return (
     <div className={styles.card}>
-      <div className={styles.imgContainer}></div>
+      <Carousel animation="slide" autoPlay={false} height={"300px"}>
+        {linkes.map((e, i) => (
+          <div
+            key={i.toString()}
+            className={styles.imgContainer}
+            style={{
+              backgroundImage: `url("${e}")`,
+            }}
+          ></div>
+        ))}
+      </Carousel>
       <h2>Ett hus</h2>
-      <h3>100 000 kr</h3>
+      <h3>97 000 - 146 000 kr</h3>
     </div>
   );
 };
--- todoz.md
+++ todoz.md
@@ -1,5 +1,7 @@
 # TODO
 
 - Swiper greier for butikk display greia
+- Legg til bilder for temp
+- Gjør komponenten generisk
 - Lag side for artikkel
--
+- idk
